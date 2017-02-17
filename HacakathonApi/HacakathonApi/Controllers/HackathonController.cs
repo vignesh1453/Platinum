da4590eb1e7a049e76edca5612b151bee5f31678
infy_602369 <infy_602369@outlook.com>
2017-02-17 11:43:24 +0530

    hackapi

---
+++ HacakathonApi/HacakathonApi/Controllers/HackathonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace HacakathonApi.Controllers
+{
+    [Route("api/Hackathon")]
+    public class HackathonController : ApiController
+    {
+        [HttpGet]
+        [Route("BuildsPerDay")]
+        public JsonResult<List<Models.BuildsCount>> BuildsPerDay()
+        {
+            var BuildLst = new List<Models.BuildsCount>();
+            var TodayDate = System.DateTime.Now.Date;
+            var date = TodayDate.Day;
+            //var obj = new Models.BuildsCounts();
+            var ran = new Random();
+            for (int i = 1; i <= date; i++)
+            {
+                var obj = new Models.BuildsCount();
+                //var ran = new Random();
+                obj.Date = TodayDate.AddDays(i).ToShortDateString();
+                obj.Success = ran.Next(1, 25);
+                obj.Failed = ran.Next(1, 25);
+                BuildLst.Add(obj);
+            }
+            //var FirstDate = 01;
+
+
+            return Json(BuildLst);
+        }
+    }
+}
---
+++ HacakathonApi/HacakathonApi/Models/BuildsCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HacakathonApi.Models
+{
+    public class BuildsCount
+    {
+        public string Date { get; set; }
+        public int Success { get; set; }
+        public int Failed { get; set; }
+    }
+}
